55f2114bfb60b2f7bd73bd2ec601062878532e5d
c2kaka <c2kaka@126.com>
2024-08-19 20:19:49 +0800

    feat: track and trigger

--- responsive-data/effect.js
+++ responsive-data/effect.js
@@ -1,28 +1,67 @@
 const app = document.getElementById("app");
 // app.innerText = data.text;
 
-const bucket = new Set();
+const bucket = new WeakMap();
 
 const data = { text: "hello, world" };
 
 const obj = new Proxy(data, {
   get(target, key) {
-    bucket.add(effect);
+    track(target, key);
     return target[key];
   },
   set(target, key, newValue) {
     target[key] = newValue;
-    bucket.forEach((fn) => fn());
+    trigger(target, key);
     return true;
   },
 });
 
-function effect() {
-  app.innerText = obj.text;
+function track(target, key) {
+  if (!activeEffect) {
+    return;
+  }
+
+  let targetDeps = bucket.get(target);
+  if (!targetDeps) {
+    targetDeps = new Map();
+    bucket.set(target, targetDeps);
+  }
+
+  let targetPropDeps = targetDeps.get(key);
+  if (!targetPropDeps) {
+    targetPropDeps = new Set();
+    targetDeps.set(key, targetPropDeps);
+  }
+
+  targetPropDeps.add(activeEffect);
 }
 
-effect(); // 触发读取
+function trigger(target, key) {
+  const targetDeps = bucket.get(target);
+  if (!targetDeps) {
+    return;
+  }
+
+  const effects = targetDeps.get(key);
+  effects &&
+    effects.forEach((effect) => {
+      effect();
+    });
+}
+
+let activeEffect;
+function effect(fn) {
+  activeEffect = fn;
+  fn();
+}
+
+effect(() => {
+  console.log("effect run");
+  app.innerText = obj.text;
+}); // 触发读取
 
 setTimeout(() => {
-  obj.text = "hello, mini-vue";
+  // obj.text = "hello, mini-vue";
+  obj.notExist = "hello, mini-vue";
 }, 1000);
